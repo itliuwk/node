12b24da165168d4d1cc7e7ece425eeb36d7208da
liuwk <1834183435@qq.com>
2019-07-24 00:07:55 +0800

    del node_modules

--- blog/bin/www.js
+++ blog/bin/www.js
@@ -3,5 +3,5 @@ const http = require('http');
 const PORT = 8000;
 const serverHandle = require('../app');
 
-const server = http.createServer(serverHandle)
-server.listen(PORT)
+const server = http.createServer(serverHandle);
+server.listen(PORT);
